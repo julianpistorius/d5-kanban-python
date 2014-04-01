b113f913aea545da3a02fd743d1e60d8c2061bbe
Robert Smallshire <robert@smallshire.org.uk>
2014-04-01 12:28:24 +0200

    Added docstrings.

--- infrastructure/event_store.py
+++ infrastructure/event_store.py
@@ -3,11 +3,26 @@ from infrastructure.transcoders import ObjectJSONEncoder, ObjectJSONDecoder
 
 
 class EventStore:
+    """A simple file-based event store which stores data in a JSON stream.
+    """
 
     def __init__(self, store_path):
+        """Open an event store.
+
+        Args:
+            store_path: THe path to a new or existing event store.
+        """
         self._store_path = store_path
 
     def append(self, topic, **attributes):
+        """Append an event.
+
+        Args:
+            topic: A string representing the event type, or topic.
+
+            **attributes: Any attributes associated with the event.
+                Attributes must be JSON serializable.
+        """
         with open(self._store_path, 'a+t') as store_file:
             event = dict(topic=topic,
                          attributes=attributes)
@@ -15,10 +30,22 @@ class EventStore:
             store_file.write('\n')
 
     def open_event_stream(self, predicate=lambda event: True):
+        """Open an event stream, optionally filtering for specific events.
+
+        Args:
+            predicate: An optional predicate function for filtering events. The predicate should
+                accept a single argument which is a deserialized JSON object, that is, a dictionary
+                with string keys and arbitrary values.
+
+        Returns:
+            An EventStream which can be used as a context manager.
+            Iteration over the EventStream yields deserialised events (dictionaries).
+        """
         return EventStream(self._store_path, predicate)
 
 
 class EventStream:
+    """A stream of events."""
 
     def __init__(self, store_path, predicate):
         self._store_path = store_path
--- utility/utilities.py
+++ utility/utilities.py
@@ -1,8 +1,18 @@
 def resolve_attr(obj, path):
-    """A recursive version of getattr for navigating dotted paths"""
+    """A recursive version of getattr for navigating dotted paths.
+
+    Args:
+        obj: An object for which we want to retrieve a nested attribute.
+        path: A dot separated string containing zero or more attribute names.
+
+    Returns:
+        The attribute referred to by obj.a1.a2.a3...
+
+    Raises:
+        AttributeError: If there is no such attribute.
+    """
     if not path:
         return obj
     head, _, tail = path.partition('.')
     head_obj = getattr(obj, head)
     return resolve_attr(head_obj, tail)
-
