80624a54c9e62c29746f4f36c14579272b505235
Robert Smallshire <robert@smallshire.org.uk>
2015-08-20 15:04:41 +0200

    Renames local predicate function _overdue() to overdue()

--- kanban/domain/services/overdue.py
+++ kanban/domain/services/overdue.py
@@ -20,11 +20,11 @@ def locate_overdue_work_items(board, work_item_repo):
     """
     today = datetime.datetime.fromtimestamp(utc_now()).date()
 
-    def _overdue(work_item):
+    def overdue(work_item):
         return (work_item.due_date is not None) and (work_item.due_date < today)
 
     work_item_ids = chain.from_iterable(column.work_item_ids() for column in board.columns())
-    overdue_work_items = work_item_repo.work_items_where(_overdue, work_item_ids)
+    overdue_work_items = work_item_repo.work_items_where(overdue, work_item_ids)
     return overdue_work_items
 
 
